1a0fac2152db8f7263f0c01baf1ac2794c63de7a
Your Name <you@example.com>
2021-03-26 11:06:38 +0800

    Initial Commit

---
+++ IRHTTPCache/IRHTTPCache.h
@@ -0,0 +1,18 @@
+//
+//  IRHTTPCache.h
+//  IRHTTPCache
+//
+//  Created by irons on 2021/3/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRHTTPCache.
+FOUNDATION_EXPORT double IRHTTPCacheVersionNumber;
+
+//! Project version string for IRHTTPCache.
+FOUNDATION_EXPORT const unsigned char IRHTTPCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRHTTPCache/PublicHeader.h>
+
+
